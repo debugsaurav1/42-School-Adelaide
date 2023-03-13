8b8a9a3495a3e11cbc57926e4fdd85a07e1c3b32
debugsaurav1 <55234906+debugsaurav1@users.noreply.github.com>
2023-03-13 11:02:04 +1030

    First upload with header in each file

---
+++ libft.h
@@ -0,0 +1,6 @@
+#ifndef __LIBFT_H__
+#define __LIBFT_H__
+
+int ft_isalnum(int c);
+
+#endif // !_LIBFT_H_
---
+++ testing/Header.h
@@ -0,0 +1,9 @@
+#ifndef Header_h
+#define Header_h
+
+#include <stdio.h>
+#include <conio.h>
+#include <Math.h>
+
+
+#endif // !Header_h
